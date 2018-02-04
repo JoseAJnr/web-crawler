fc97cd4f1c6dc0fa3eb8f5a892ca541a8bf79a9e
Junior Almeida <jporanga@users.noreply.github.com>
2018-02-04 00:43:35 -0300

    Fix for ul tag

--- web-crawler.py
+++ web-crawler.py
@@ -1,8 +1,43 @@
 import requests
 import json
-import argparse
 from xml.etree import ElementTree
 from bs4 import BeautifulSoup
+
+
+def _treat_parag(tag):
+    if not tag.text.replace('\t', '').replace('\xa0', '').strip('\n'):
+        return
+    return dict(type='text', content=tag.text.replace('\t', '').replace('\xa0', '').strip('\n'))
+
+
+def _treat_img(tag):
+    return dict(type='image', content=tag['src'])
+
+
+def _treat_ul(tag):
+    result = dict(type='links', source=[])
+    for link in tag.find_all('a'):
+        result['source'].append(link['href'])
+
+    return result
+
+
+TREAT_TAGS = {
+    'p': _treat_parag,
+    'img': _treat_img,
+    'ul': _treat_ul
+}
+
+
+def _tag_to_dict(element):
+    """
+    Method to categorize the elements from description
+    :param bs4.element.Tag element: The element tag that must have the info extracted
+    :return dict: A dictionary with all info treated
+    """
+    if not element.name:
+        return
+    return TREAT_TAGS.get(element.name)(element) if TREAT_TAGS.get(element.name) else None
 
 
 def treat_description(description):
@@ -12,45 +47,21 @@ def treat_description(description):
     :return list[dict]: A list of dictionaries with all tag content treated
     """
     result = list()
-    links_dict = dict(type='links', content=[])
     desc_html = BeautifulSoup(description, 'html.parser')
     for tag in desc_html.find_all():
         type_dict = _tag_to_dict(tag)
         if type_dict:
-            if tag.name != 'a':
-                result.append(type_dict)
-            else:
-                links_dict['content'].append(type_dict)
+            result.append(type_dict)
 
-    result.append(links_dict)
-
-    return result
-
-
-def _tag_to_dict(element):
-    """
-    Method to categorize the elements from description
-    :param bs4.element.Tag element: The element tag that must have the info extracted
-    :return dict: A dictionary with all info treated
-    """
-    if element.name == 'a':
-        result = element['href']
-    elif element.name == 'img':
-        result = dict(type='image', content=element['src'])
-    # Eliminating lines without content
-    elif element.name == 'p' and element.text.replace('\t', '').replace('\xa0', '').strip('\n'):
-        result = dict(type='text', content=element.text.replace('\t', '').replace('\xa0', '').strip('\n'))
-    else:
-        return
     return result
 
 
 def main():
     # Making the request to the feed URL
-    data = requests.get('http://revistaautoesporte.globo.com/rss/ultimas/feed.xml')
+    feed_data = requests.get('http://revistaautoesporte.globo.com/rss/ultimas/feed.xml')
 
     # Transforming the text in a xml object
-    tree = ElementTree.ElementTree(ElementTree.fromstring(data.text))
+    tree = ElementTree.ElementTree(ElementTree.fromstring(feed_data.text))
 
     # Getting XML root
     root = tree.getroot()
